dcb96d24f016ae9b4a08318a0d75df16bdca4b86
apilaka <apilaka@yahoo.ca>
2022-10-12 13:05:14 -0400

    first commit

---
+++ src/main/java/com/pilaka/JenkinTest/JenkinTestApplication.java
@@ -0,0 +1,26 @@
+package com.pilaka.JenkinTest;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinTestApplication {
+
+	static org.slf4j.Logger log = LoggerFactory.getLogger(JenkinTestApplication.class);
+	
+	@PostConstruct
+	static void init() {
+		log.info("application started");
+	}
+	public static void main(String[] args) {
+		log.info("application executed");	log.info("executing test class");
+		SpringApplication.run(JenkinTestApplication.class, args);
+		init();
+	}
+
+}
---
+++ src/test/java/com/pilaka/JenkinTest/JenkinTestApplicationTests.java
@@ -0,0 +1,22 @@
+package com.pilaka.JenkinTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.slf4j.LoggerFactory;
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class JenkinTestApplicationTests {
+
+	static org.slf4j.Logger  log = LoggerFactory.getLogger(JenkinTestApplicationTests.class);
+	@Test
+	void contextLoads() {
+	
+		assertEquals(true, true);
+	}
+
+}
